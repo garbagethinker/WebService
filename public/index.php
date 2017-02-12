710f7dfe52835eba36bc3243bf9cf8ca20cb2d8b
Bhupendra Yadav <garbagethinker@gmail.com>
2017-02-12 19:05:36 +0530

    renamed the main content area

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
     <body>
         <div id="header">This is the header</div>
         <div id="navbar">Navbar</div>
-        <div id="main_content">Main Content</div>
+        <div id="main_content">Website Actual Content</div>
         <div id="sidebar">Sidebar</div>
         <div id="footer">Footer</div>
     </body>
