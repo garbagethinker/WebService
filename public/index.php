81e9c1fea069eb56aeffefbf6ed5c20c0d72b67f
Bhupendra Yadav <garbagethinker@gmail.com>
2017-02-12 19:06:39 +0530

    renamed the right sidebar area

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@
         <div id="header">This is the header</div>
         <div id="navbar">Navbar</div>
         <div id="main_content">Website Actual Content</div>
-        <div id="sidebar">Sidebar</div>
+        <div id="sidebar">Right Sidebar</div>
         <div id="footer">Footer</div>
     </body>
 </html>
