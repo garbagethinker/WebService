3bb1d8c07ac2090b35744a5f8534c39a9665d662
Bhupendra Singh <bhupendra.singh@appstreet.io>
2017-02-12 20:58:52 +0530

    changed name of main content

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
     <body>
         <div id="header">This is the header</div>
         <div id="navbar">Navbar</div>
-        <div id="main_content">Website Actual Content</div>
+        <div id="main_content">Website Main Content</div>
         <div id="sidebar">Right Sidebar</div>
         <div id="footer">Footer</div>
     </body>
